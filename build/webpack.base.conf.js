800d0fa017580e0961a3fe95e1809a8549ce6f5f
fanxiaolong <fanxiaolongfe@gmail.com>
2016-09-23 20:54:00 +0800

    全局注册query

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -18,7 +18,8 @@ module.exports = {
     alias: {
       'src': path.resolve(__dirname, '../src'),
       'assets': path.resolve(__dirname, '../src/assets'),
-      'components': path.resolve(__dirname, '../src/components')
+      'components': path.resolve(__dirname, '../src/components'),
+      'jquery': path.resolve(__dirname, '../node_modules/jquery/dist/jquery.js')
     }
   },
   resolveLoader: {
@@ -59,7 +60,10 @@ module.exports = {
           limit: 10000,
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
-      }
+      }, {
+        test: require.resolve('jquery'),
+        loader: 'expose?jQuery!expose?$'
+       }
     ]
   },
   vue: {
