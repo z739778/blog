317514965fba6d5330dba9ca93808a9ed35279bc
fanxiaolong <fanxiaolongfe@gmail.com>
2016-09-24 13:59:14 +0800

    删除依赖

--- src/main.js
+++ src/main.js
@@ -1,15 +1,6 @@
 require('font-awesome/css/font-awesome.css')
 require('simple-line-icons/css/simple-line-icons.css')
 require('bootstrap/dist/css/bootstrap.css')
-require('bootstrap-switch/dist/css/bootstrap3/bootstrap-switch.css')
-require('bootstrap-datepicker/dist/css/bootstrap-datepicker.css')
-require('jquery')
-require('bootstrap/dist/js/bootstrap')
-require('bootstrap-hover-dropdown/bootstrap-hover-dropdown')
-require('bootstrap-switch/dist/js/bootstrap-switch')
-require('bootstrap-datepicker/dist/js/bootstrap-datepicker')
-require('bootstrap-datepicker/dist/locales/bootstrap-datepicker.zh-CN.min')
-require('bootstrap-timepicker/js/bootstrap-timepicker')
 
 import Vue from 'vue'
 import App from './App'
