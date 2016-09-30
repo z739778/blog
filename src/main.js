bea063551dad9d7b3970f69939c5a6a93a64ef79
fanxiaolong <fanxiaolongfe@gmail.com>
2016-09-30 16:27:04 +0800

    精简依赖

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 require('font-awesome/css/font-awesome.css')
 require('simple-line-icons/css/simple-line-icons.css')
-require('bootstrap/dist/css/bootstrap.css')
 
 import Vue from 'vue'
 import App from './App'
